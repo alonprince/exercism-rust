661707931ee8f3bb2b5bf19f655ca2b178426446
wang.heng <wang.heng@immomo.com>
2017-12-27 18:10:06 +0800

    beer-song

---
+++ beer-song/src/main.rs
@@ -0,0 +1,5 @@
+extern crate beer_song as beer;
+
+fn main() {
+  assert_eq!(beer::sing(3, 0), "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+}
