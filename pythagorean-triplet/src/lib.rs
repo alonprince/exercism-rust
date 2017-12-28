47aba025b391c98a906b8d90a3443e9640dc6199
wang.heng <wang.heng@immomo.com>
2017-12-28 19:23:13 +0800

    optimize

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,19 +1,12 @@
 pub fn find() -> Option<u32> {
-    let mut res: u32 = 0;
     for a in 1..999 {
-        if a > 997 {
-            return None;
-        }
         for b in 1..999 - a {
             let c = 1000 - a - b;
             if (a as u32).pow(2) + (b as u32).pow(2) == (c as u32).pow(2) {
-                res = a * b * c;
-                break;
+                let res: u32 = a * b * c;
+                return Some(res);
             }
         }
-        if res != 0 {
-            break;
-        }
     }
-    Some(res)
+    None
 }
